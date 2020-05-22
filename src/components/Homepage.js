d3678d7df922d93a893b48b62df8646200fe0f67
sjsig <steviejoesig49@gmail.com>
2020-05-22 16:35:00 -0400

    Added page structure

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { apiCall } from "../services/api";
 
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
   }
-
+  async componentDidMount() {
+    let userData = await apiCall("get", `/api/`);
+    alert(userData.message);
+  }
   render() {
     return (
       <div>
