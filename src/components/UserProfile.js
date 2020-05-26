9a7f495b79fff0b2e11402b1480074e76779de58
sjsig <steviejoesig49@gmail.com>
2020-05-26 13:33:52 -0400

    Property deletion implemented

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -18,20 +18,36 @@ class UserProfile extends React.Component {
   async componentDidMount() {
     let userData = await apiCall("get", `/api/users/${this.props.currentUser.user.userId}`);
     let transactionData = await apiCall("get", `/api/user/${this.props.currentUser.user.userId}/transaction`);
-    let propertiesData = await apiCall("get", `/api/property/landlord/${this.props.currentUser.user.userId}`);
     let leaseData = await apiCall("get", `/api/lease/${this.props.currentUser.user.userId}`);
     this.setState({
       userInfo: userData.user,
       transactionHistory: transactionData.transactions,
-      properties: propertiesData.properties,
       lease: leaseData.lease,
     });
-    console.log("User state:", this.state);
+
+    let propertiesData = await apiCall("get", `/api/property/landlord/${this.props.currentUser.user.userId}`);
+    let unitsAdded = await propertiesData.properties.map(async (property) => {
+      let unitData = await apiCall("get", `/api/property/${property.property_id}/unit`);
+      return { ...property, units: unitData.units };
+    });
+    Promise.all(unitsAdded).then((values) => {
+      this.setState({ properties: values });
+    });
   }
+
+  testButton = () => {
+    console.log("current state:", this.state);
+  };
+
   deleteUser = () => {
     apiCall("delete", `/api/users/${this.props.currentUser.user.userId}`);
     this.props.logout();
     this.props.history.push("/");
+  };
+
+  deleteProperty = (propertyId) => {
+    apiCall("delete", `/api/property/${propertyId}`);
+    this.props.history.push(`/users/${this.props.currentUser.user.userId}`);
   };
 
   terminateLease = () => {
@@ -39,15 +55,24 @@ class UserProfile extends React.Component {
     this.props.history.push(`/users/${this.props.currentUser.user.userId}`);
   };
   render() {
-    let properties = this.state.properties.map((property) => (
-      <li>
-        <div>
-          {property.address}, {property.city}
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-      </li>
-    ));
+    let properties = this.state.properties.map((property) => {
+      let unitList = property.units.map((unit) => (
+        <li>
+          Unit {unit.unit_number}, {unit.market_price} per month,{" "}
+          {unit.is_occupied == 1 ? <em>occupied</em> : <em>unoccupied</em>}
+        </li>
+      ));
+      return (
+        <li>
+          <div>
+            {property.address}, {property.city}
+            <ul>{unitList}</ul>
+            <button>Edit</button>
+            <button onClick={() => this.deleteProperty(property.property_id)}>Delete</button>
+          </div>
+        </li>
+      );
+    });
     return (
       <div>
         <Navbar />
@@ -77,8 +102,8 @@ class UserProfile extends React.Component {
           </div>
         )}
         <h1>You have {this.state.transactionHistory.length} transactions in your history</h1>
-
         <button onClick={this.deleteUser}>Delete Account</button>
+        <button onClick={this.testButton}>Console.log current state</button>
       </div>
     );
   }
